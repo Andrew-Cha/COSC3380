afcda15d5bb91e14e1a7bc5b5d9800970ac0c461
Richard Hernandez <149026632+tixxxa@users.noreply.github.com>
2023-11-14 19:46:23 -0600

    added endpoints for fines and pastLoans

--- backend/endpoints.js
+++ backend/endpoints.js
@@ -184,6 +184,115 @@ app.post('/api/fine', async (req, res) => {
         res.status(500).json({ error: 'Database error' });
     }
 });
+app.get('/api/reports/fines', async (req, res) => {
+    try {
+      const { period } = req.query;
+      let startDate;
+  
+      if (period === 'lastDay') {
+        startDate = new Date();
+        startDate.setDate(startDate.getDate() - 1);
+      } else if (period === 'lastWeek') {
+        startDate = new Date();
+        startDate.setDate(startDate.getDate() - 7);
+      } else if (period === 'lastMonth') {
+        startDate = new Date();
+        startDate.setMonth(startDate.getMonth() - 1);
+      } else if (period === 'lastYear') {
+        startDate = new Date();
+        startDate.setFullYear(startDate.getFullYear() - 1);
+      } else {
+        return res.status(400).json({ error: 'Invalid period' });
+      }
+  
+      const query = `
+        SELECT SUM(fine_amount) AS total_fines
+        FROM fine
+        WHERE fined_at >= $1;
+      `;
+      
+      const result = await pool.query(query, [startDate]);
+  
+      res.json(result.rows[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+  app.get('/api/reports/pastLoans', async (req, res) => {
+    try {
+      const { period } = req.query;
+      let startDate;
+  
+      if (period === 'lastDay') {
+        startDate = new Date();
+        startDate.setDate(startDate.getDate() - 1);
+      } else if (period === 'lastWeek') {
+        startDate = new Date();
+        startDate.setDate(startDate.getDate() - 7);
+      } else if (period === 'lastMonth') {
+        startDate = new Date();
+        startDate.setMonth(startDate.getMonth() - 1);
+      } else if (period === 'lastYear') {
+        startDate = new Date();
+        startDate.setFullYear(startDate.getFullYear() - 1);
+      } else {
+        return res.status(400).json({ error: 'Invalid period' });
+      }
+  
+      const query = `
+      SELECT
+        c.first_name || ' ' || c.last_name AS customer_name,
+        'book' AS item_type,
+        b.title AS item_name,
+        b2c.returned_at AS date
+      FROM
+        book_to_customer b2c
+      JOIN
+        customer c ON b2c.customer_id = c.id
+      JOIN
+        book b ON b2c.book_id = b.id
+      WHERE
+        b2c.returned_at >= $1
+      UNION
+      SELECT
+        c.first_name || ' ' || c.last_name AS customer_name,
+        'media' AS item_type,
+        m.title AS item_name,
+        m2c.returned_at AS date
+      FROM
+        media_to_customer m2c
+      JOIN
+        customer c ON m2c.customer_id = c.id
+      JOIN
+        media m ON m2c.media_id = m.id
+      WHERE
+        m2c.returned_at >= $1
+      UNION
+      SELECT
+        c.first_name || ' ' || c.last_name AS customer_name,
+        'device' AS item_type,
+        d.device_name AS item_name,
+        d2c.returned_at AS date
+      FROM
+        device_to_customer d2c
+      JOIN
+        customer c ON d2c.customer_id = c.id
+      JOIN
+        device d ON d2c.device_id = d.id
+      WHERE
+        d2c.returned_at >= $1;
+    `;
+      
+      const result = await pool.query(query, [startDate]);
+  
+      res.json(result.rows[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
 
 app.listen(3000, process.env.VITE_SERVER_URL, async () => {
     await pool.query(initializeQuery);
